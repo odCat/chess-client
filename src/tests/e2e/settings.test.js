6a79becf1853eb04e2f249e1a79390a5edd2426a
Mihai Gătejescu <mgatejescu@pm.me>
2026-08-25 18:39:57 +0300

    Cancel account deletion

--- src/tests/e2e/settings.test.js
+++ src/tests/e2e/settings.test.js
@@ -86,3 +86,29 @@ test("can delete account", async ({ page }) => {
 
     await expect(page.getByText("Invalid username or password")).toHaveCount(2);
 })
+
+test("can cancel account deletion", async ({ page }) => {
+    const registration = await registerNewPlayer();
+    const player = await registration.input;
+    await page.goto("http://localhost:5173/login")
+    await page.getByRole('textbox', { name: /^Email\/Username$/ }).fill(player.username);
+    await page.getByRole('textbox', { name: /^Password$/ }).fill(player.password);
+    await page.getByRole("button", { name: /^Login$/ }).click();
+    await page.getByRole("button", { name: player.username }).click();
+    await page.getByRole("menuitem", { name: "Settings" }).click();
+
+    await page.getByRole("button", { name: "Delete your account" }).click();
+
+    expect(page.getByRole("heading", { name: "Are you sure you want to delete your account?"}));
+    expect(page.getByText("This action cannot be undone."));
+    expect(page.getByRole("button", { name: "Delete"}));
+    expect(page.getByRole("button", { name: "Cancel"}));
+    await page.getByRole("button", { name: "Cancel" }).click();
+
+    await expect(page).toHaveURL("http://localhost:5173/settings");
+
+    const login = await loginPlayer(player.username, player.password);
+    expect(login.status()).toBe(200);
+
+    await deletePlayer({ usernameOrEmail: player.username, password: player.password });
+})
